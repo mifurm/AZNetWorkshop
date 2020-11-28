0ff01a3fa49e89fb1df28b0211dde910852156e6
mfurmankiewicz <michal.furmankiewicz@chmurowisko.pl>
2020-11-28 08:42:10 +0100

    lab

---
+++ 0.main.tf
@@ -0,0 +1,20 @@
+###
+### TODO: Setup Your provider
+###
+provider "azurerm" {
+
+  #TODO
+  subscription_id= "<SUB ID>"
+  
+  #TODO
+  tenant_id="<TENANT ID>"
+  
+  #TODO
+  client_id="<APP ID>"
+  
+  #TODO
+  client_secret="<SECRET>"
+  
+  version = "=2.30.0"
+  features {}
+}
