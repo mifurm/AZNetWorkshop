ff68e56b30424ef072e4118c6baec238fbf71e2f
mifurm <michal.furmankiewicz@hotmail.com>
2023-04-27 23:09:32 +0200

    Update 0.main.tf

--- MAIN/0.main.tf
+++ MAIN/0.main.tf
@@ -2,7 +2,7 @@
 ### TODO: Setup Your provider
 ###
 provider "azurerm" {
-  subscription_id = "7a3ff142-984a-4537-ad5a-cd0ace174e69"
+  subscription_id = "4dc4b888-e3bc-4c76-951b-4ece151e3f19"
   features {}
 }
 
